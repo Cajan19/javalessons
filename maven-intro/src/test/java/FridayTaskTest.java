35a4381aaa6265c9aef1326fa111f4a568f0b363
Carolin Janshen <caro.janshen@gmail.com>
2020-05-15 14:03:22 +0200

    Add updated files

--- maven-intro/src/test/java/FridayTaskTest.java
+++ maven-intro/src/test/java/FridayTaskTest.java
@@ -1,5 +1,54 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FridayTaskTest {
+
+    @Test
+    public void isNotLongEnough() {
+
+//        given
+        String aPassword = "ushs";
+
+//              WHEN
+
+        boolean result = FridayTask.passwordIsSecureOrNot(aPassword);
+//        THEN
+
+        assertFalse(result);
+    }
+
+
+
+
+    @Test
+    public void atLeastOneDigit() {
+//        given
+        String thePassword = "hsdjhafdhj";
+
+//        when
+
+        boolean result = FridayTask.passwordIsSecureOrNot(thePassword);
+
+//        then
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void passwordMeetsRequirements() {
+
+//        given
+
+        String password = "kdsjhfksajhfkajsdfh777";
+
+//        when
+
+        boolean result = FridayTask.passwordIsSecureOrNot(password);
+
+//        then
+
+        assertTrue(result);
+    }
 
 }
